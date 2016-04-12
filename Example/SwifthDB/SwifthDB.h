23e32e8354efa9745db0eade71d1bba2d01e411e
bob <pockettutor@qq.com>
2016-04-12 10:57:38 +0800

    add carthage support

---
+++ Example/SwifthDB/SwifthDB.h
@@ -0,0 +1,19 @@
+//
+//  SwifthDB.h
+//  SwifthDB
+//
+//  Created by bob on 16/4/12.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwifthDB.
+FOUNDATION_EXPORT double SwifthDBVersionNumber;
+
+//! Project version string for SwifthDB.
+FOUNDATION_EXPORT const unsigned char SwifthDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwifthDB/PublicHeader.h>
+
+
---
+++ Pod/Classes/SwifthDB.h
@@ -0,0 +1,13 @@
+//
+//  Swifth.h
+//  SwifthDB
+//
+//  Created by bob on 16/4/12.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+#ifndef SwifthDB_h
+#define SwifthDB_h
+
+
+#endif /* SwifthDB_h */
